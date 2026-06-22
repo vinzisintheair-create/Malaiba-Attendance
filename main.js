6b9e62046aebb65df2b9c4991c7ef86af472f842
Bai Bens <vinzisintheair@gmail.com>
2026-06-22 18:21:04 +0800

    Configure Electron wrapper and script packaging options for desktop standalone build

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true
+    },
+    autoHideMenuBar: true,
+    title: "Classroom Pulse - Malaiba Elementary School",
+    icon: path.join(__dirname, 'assets/icon.png')
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+  
+  // Open external links in default browser instead of inside electron window
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    const { shell } = require('electron');
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
